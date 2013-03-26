f2a851189faa0ad5778d0a91aba87516aaecb860
moro <moronatural@gmail.com>
2013-03-26 15:43:49 +0900

    Setup capybara and poltergeist

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,9 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
@@ -14,4 +17,11 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = 'random'
+
+  config.include Capybara::DSL
 end
+
+Capybara.default_selector        = :css
+Capybara.save_and_open_page_path = File.dirname(__FILE__) + '/../tmp/save_and_open_page'
+Capybara.default_driver          = :poltergeist
+
